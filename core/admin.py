ab51c4fb248189e5dc383d4eb28efabfcf79be51
Ihor Palii <palii.ihor@uvik.net>
2026-04-29 10:12:26 +0300

    Models done

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import MeasurementUnit, Product, Shop, Receipt, StockItem, Purchase
+
+admin.site.site_header = "Рецепти"
+admin.site.site_title = "Рецепти"
+admin.site.index_title = "Вітаємо на сторінці"
+
+
+@admin.register(MeasurementUnit)
+class MeasurementUnitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_name')
+    search_fields = ('name', 'short_name')
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+    search_fields = ('name', 'address')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'code', 'measurement_unit')
+    search_fields = ('title', 'code')
+
+
+@admin.register(Receipt)
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    search_fields = ('title', 'description')
+
+
+class StockItemsInline(admin.TabularInline):
+    model = StockItem
+    extra = 1
+
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ('date', 'total_price')
+    search_fields = ('date', 'total_price')
+    inlines = [StockItemsInline]
--- core/models.py
+++ core/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class MeasurementUnit(models.Model):
-    name = models.CharField(max_length=50)
-    short_name = models.CharField(max_length=10)
+    name = models.CharField(max_length=50, verbose_name="Назва")
+    short_name = models.CharField(max_length=10, verbose_name="Коротка назва")
 
     def __str__(self):
         return self.name
@@ -14,9 +14,9 @@ class MeasurementUnit(models.Model):
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=255)
-    code = models.CharField(max_length=255)
-    measurement_unit = models.ForeignKey(MeasurementUnit, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name="Назва")
+    code = models.CharField(max_length=255, verbose_name="Код")
+    measurement_unit = models.ForeignKey(MeasurementUnit, on_delete=models.CASCADE, verbose_name="Одиниця вимірювання")
 
     def __str__(self):
         return self.title
@@ -27,9 +27,9 @@ class Product(models.Model):
 
 
 class Receipt(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    products = models.ManyToManyField(Product)
+    title = models.CharField(max_length=255, verbose_name="Назва")
+    description = models.TextField(verbose_name="Опис")
+    products = models.ManyToManyField(Product, verbose_name="Продукти")
 
     def __str__(self):
         return self.title
@@ -40,11 +40,12 @@ class Receipt(models.Model):
 
 
 class StockItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    expiration_date = models.DateField(null=True, blank=True)
-    out_of_stock = models.BooleanField(default=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Продукт")
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Ціна")
+    amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Кількість")
+    expiration_date = models.DateField(null=True, blank=True, verbose_name="Термін придатності")
+    out_of_stock = models.BooleanField(default=False, verbose_name="Відсутність")
+    purchase = models.ForeignKey("Purchase", on_delete=models.CASCADE, null=True, blank=True, verbose_name="Покупка")
 
     def __str__(self):
         return f"{self.product.title} - {self.amount}"
@@ -55,10 +56,9 @@ class StockItem(models.Model):
 
 
 class Purchase(models.Model):
-    shop = models.ForeignKey('Shop', on_delete=models.CASCADE, null=True, blank=True)
-    items = models.ManyToManyField(StockItem)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField()
+    shop = models.ForeignKey('Shop', on_delete=models.CASCADE, null=True, blank=True, verbose_name="Магазин")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Загальна ціна")
+    date = models.DateField(verbose_name="Дата")
 
     def __str__(self):
         return f"Purchase on {self.date} - Total: ${self.total_price}"
@@ -69,8 +69,8 @@ class Purchase(models.Model):
 
 
 class Shop(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255, verbose_name="Назва")
+    address = models.CharField(max_length=255, blank=True, null=True, verbose_name="Адреса")
 
     def __str__(self):
         return self.name
